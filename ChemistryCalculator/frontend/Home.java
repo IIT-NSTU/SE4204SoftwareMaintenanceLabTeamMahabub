98e2d36a03fd81a77851a567f5a2b5a750c353e3
s96Abrar <s96mini.cube@gmail.com>
2023-02-28 20:24:34 +0600

    Fixed Divergent change, Long method by extract method

--- ChemistryCalculator/frontend/Home.java
+++ ChemistryCalculator/frontend/Home.java
@@ -2,6 +2,10 @@ package ChemistryCalculator.frontend;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Home extends JFrame {
 
@@ -10,7 +14,7 @@ public class Home extends JFrame {
     private final Sidebar sidebarPanel = new Sidebar();
 
 
-    //body panels
+    // Available body panels
     private final JPanel equationBalancePanel = new EquationBalancePanel();
     private final JPanel concentrationPanel = new ConcentrationPanel();
     private final JPanel molarMassPanel = new MolarMassPanel();
@@ -19,37 +23,51 @@ public class Home extends JFrame {
     private final JPanel percentOfCompletionPanel = new PercentOfCompletionPanel();
     private final JPanel titrationPanel = new TitrationPanel();
 
-
     public Home() {
-        buildSidebar();
-        buildBodyPanels();
-        buildBackgroundPanel();
-        setFrameProperty();
+        ArrayList<JPanel> bodyPanels = new ArrayList<>(
+                List.of(equationBalancePanel, concentrationPanel, electronConfigPanel, molarMassPanel, percentOfCompletionPanel, titrationPanel, needHelpPanel)
+        );
+
+        initializeSidebar(bodyPanels);
+        initializeBodyPanelLayout(bodyPanels);
+        backgroundPanel.setLayout(getBackgroundPanelLayout(backgroundPanel));
+        getContentPane().setLayout(getMainPanelLayout(getContentPane()));
+        setupFrameProperties();
     }
 
-    public static void main(String[] args) {
-        // Set the Nimbus look and feel
-        try {
-            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException |
-                 UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Home.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+    private void initializeBodyPanelLayout(ArrayList<JPanel> bodyPanels) {
+        bodyPanel.setLayout(new CardLayout());
+        for (JPanel panel : bodyPanels) {
+            bodyPanel.add(panel);
+        }
+    }
+
+    private void initializeSidebar(ArrayList<JPanel> bodyPanels) {
+        ArrayList<String> titles = new ArrayList<>(
+                List.of("Equation Balance", "Concentration", "Electron config", "Molar mass", "Percent of completion", "Titration", "Need help ?")
+        );
+
+        ArrayList<String> icons = new ArrayList<>(
+                List.of(
+                        "icons8_scales_25px_1.png", "icons8_dna_helix_25px.png", "icons8_physics_25px.png", "icons8_weightlifting_25px.png",
+                        "icons8_percentage_25px.png", "icons8_test_tube_25px.png", "icons8_inquiry_25px_1.png"
+                )
+        );
+
+        for (int i = 0; i < bodyPanels.size(); i++) {
+            sidebarPanel.addMenu(titles.get(i), new ImageIcon(getClass().getResource("/ChemistryCalculator/icons/" + icons.get(i))), bodyPanels.get(i));
         }
 
-        // Create and display the form
-        EventQueue.invokeLater(() -> new Home().setVisible(true));
+        sidebarPanel.build();
     }
 
-    private void buildBackgroundPanel() {
-        //attaching body panel and sidebar panel to the background panel
-
-        GroupLayout backgroundPanelLayout = new GroupLayout(backgroundPanel);
-        backgroundPanel.setLayout(backgroundPanelLayout);
+    /**
+     * Group sidebarPanel and bodyPanel into the given container using GroupLayout
+     * @param container In which container to add the sidebarPanel and bodyPanel
+     * @return GroupLayout with sidebarPanel and bodyPanel
+     */
+    private GroupLayout getBackgroundPanelLayout(Container container) {
+        GroupLayout backgroundPanelLayout = new GroupLayout(container);
         backgroundPanelLayout.setHorizontalGroup(
                 backgroundPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
                         .addGroup(backgroundPanelLayout.createSequentialGroup()
@@ -62,76 +80,28 @@ public class Home extends JFrame {
                         .addComponent(sidebarPanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(bodyPanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
+        return backgroundPanelLayout;
     }
 
-    private void buildBodyPanels() {
-        //adding all body panels to a single panel (bodyPanel)
-        bodyPanel.setLayout(new CardLayout());
-        bodyPanel.add(equationBalancePanel);
-        bodyPanel.add(concentrationPanel);
-        bodyPanel.add(molarMassPanel);
-        bodyPanel.add(electronConfigPanel);
-        bodyPanel.add(titrationPanel);
-        bodyPanel.add(needHelpPanel);
-        bodyPanel.add(percentOfCompletionPanel);
-    }
-
-    private void buildSidebar() {
-        // creating sidebar. Every single body panel should have a single menu bar for navigation. Don't create duplicates.
-        sidebarPanel.addMenu(
-                "Equation Balance",
-                new ImageIcon(getClass().getResource("/ChemistryCalculator/icons/icons8_scales_25px_1.png")),
-                equationBalancePanel
-        );
-        sidebarPanel.addMenu(
-                "Concentration",
-                new ImageIcon(getClass().getResource("/ChemistryCalculator/icons/icons8_dna_helix_25px.png")),
-                concentrationPanel
-        );
-        sidebarPanel.addMenu(
-                "Electron config",
-                new ImageIcon(getClass().getResource("/ChemistryCalculator/icons/icons8_physics_25px.png")),
-                electronConfigPanel
-        );
-        sidebarPanel.addMenu(
-                "Molar mass",
-                new ImageIcon(getClass().getResource("/ChemistryCalculator/icons/icons8_weightlifting_25px.png")),
-                molarMassPanel
-        );
-        sidebarPanel.addMenu(
-                "Percent of completion",
-                new ImageIcon(getClass().getResource("/ChemistryCalculator/icons/icons8_percentage_25px.png")),
-                percentOfCompletionPanel
-        );
-        sidebarPanel.addMenu(
-                "Titration",
-                new ImageIcon(getClass().getResource("/ChemistryCalculator/icons/icons8_test_tube_25px.png")),
-                titrationPanel
-        );
-        sidebarPanel.addMenu(
-                "Need help ?",
-                new ImageIcon(getClass().getResource("/ChemistryCalculator/icons/icons8_inquiry_25px_1.png")),
-                needHelpPanel
-        );
-
-        sidebarPanel.build();
-    }
-
-    private void setFrameProperty() {
-        //attaching background Panel to the main ContentPane.
-        GroupLayout layout = new GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-                layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+    /**
+     * Group backgroundPanel into the given container using GroupLayout
+     * @param container In which container to add backgroundPanel
+     * @return GroupLayout with backgroundPanel
+     */
+    private GroupLayout getMainPanelLayout(Container container) {
+        GroupLayout mainPanelLayout = new GroupLayout(container);
+        mainPanelLayout.setHorizontalGroup(
+                mainPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
                         .addComponent(backgroundPanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
-        layout.setVerticalGroup(
-                layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+        mainPanelLayout.setVerticalGroup(
+                mainPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
                         .addComponent(backgroundPanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
+        return mainPanelLayout;
+    }
 
-        //setting frame properties
-
+    private void setupFrameProperties() {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle("Chemistry Calculator");
         setAlwaysOnTop(true);
@@ -142,4 +112,24 @@ public class Home extends JFrame {
         pack();
         setLocationRelativeTo(null);
     }
+
+    public static void setLookAndFeel(String lookAndFeelName) {
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if (lookAndFeelName.equalsIgnoreCase(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException |
+                 UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(Home.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static void main(String[] args) {
+        setLookAndFeel("Nimbus");
+        EventQueue.invokeLater(() -> new Home().setVisible(true));
+    }
+
 }
