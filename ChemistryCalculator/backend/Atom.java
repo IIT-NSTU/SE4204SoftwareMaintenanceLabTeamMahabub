272c32ab40dc46e53339317764534a83c8f27780
s96Abrar <s96mini.cube@gmail.com>
2023-02-28 15:45:37 +0600

    Fixed primitive obsession for atom

--- ChemistryCalculator/backend/Atom.java
+++ ChemistryCalculator/backend/Atom.java
@@ -14,36 +14,7 @@ public class Atom {
     }
 
     public String getElectronConfig() {
-        int counter;
-        int tempAtomicNumber = this.atomicNumber;
-
-        StringBuilder output = new StringBuilder();
-        String[][] orbitals = {
-                {"1s", "2"}, {"2s", "2"}, {"2p", "6"},
-                {"3s", "2"}, {"3p", "6"}, {"4s", "2"},
-                {"3d", "10"}, {"4p", "6"}, {"5s", "2"},
-                {"4d", "10"}, {"5p", "6"}, {"6s", "2"},
-                {"4f", "14"}, {"5d", "10"}, {"6p", "6"},
-                {"7s", "2"}, {"5f", "14"}, {"6d", "10"},
-                {"7p", "6"}, {"8s", "2"}
-        };
-
-        for (String[] innerArray : orbitals) {
-            String orbitalName = innerArray[0];
-            int orbitalSize = Integer.parseInt(innerArray[1]);
-
-            counter = Math.min(orbitalSize, tempAtomicNumber);
-
-            output.append(orbitalName).append(counter).append(" ");
-
-            tempAtomicNumber -= counter;
-            if (tempAtomicNumber <= 0) {
-                break;
-            }
-
-        }
-
-        return output.toString();
+        return Orbitals.electronConfigForAtom(this.atomicNumber);
     }
 
     public String getSymbol() {
@@ -66,5 +37,6 @@ public class Atom {
     public String toString() {
         return String.format("Atom {%s %s %f %d}", name, symbol, atomicMass, atomicNumber);
     }
+
 }
 
---
+++ ChemistryCalculator/backend/Orbitals.java
@@ -0,0 +1,77 @@
+package ChemistryCalculator.backend;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Orbitals {
+    private static ArrayList<Orbital> atomicOrbitals;
+    static {
+        atomicOrbitals = new ArrayList<>(
+                List.of(
+                        new Orbital(1, "s", 2),
+                        new Orbital(2, "s", 2),
+                        new Orbital(2, "p", 6),
+                        new Orbital(3, "s", 2),
+                        new Orbital(3, "p", 6),
+                        new Orbital(3, "d", 10),
+                        new Orbital(4, "s", 2),
+                        new Orbital(4, "p", 6),
+                        new Orbital(4, "d", 10),
+                        new Orbital(4, "f", 14),
+                        new Orbital(5, "s", 2),
+                        new Orbital(5, "p", 6),
+                        new Orbital(5, "d", 10),
+                        new Orbital(5, "f", 14),
+                        new Orbital(6, "s", 2),
+                        new Orbital(6, "p", 6),
+                        new Orbital(6, "d", 10),
+                        new Orbital(7, "s", 2),
+                        new Orbital(7, "p", 6),
+                        new Orbital(8, "s", 2)
+                )
+        );
+    }
+
+    public static String electronConfigForAtom(int atomicNumber) {
+        StringBuilder sb = new StringBuilder();
+        if (AtomDatabase.isValidAtom(atomicNumber)) {
+            int counter;
+            int tempAtomicNumber = atomicNumber;
+
+            for (Orbital orbital : atomicOrbitals) {
+                counter = Math.min(orbital.getNumberOfElectron(), tempAtomicNumber);
+                sb.append(orbital.getEnergyLevel()).append(orbital.getOrbitalType()).append(counter).append(" ");
+
+                tempAtomicNumber -= counter;
+                if (tempAtomicNumber <= 0) {
+                    break;
+                }
+            }
+        }
+        return sb.toString();
+    }
+
+    private static class Orbital {
+        private final String orbitalType;
+        private final int energyLevel;
+        private final int numberOfElectron;
+
+        public Orbital(int energyLevel, String orbitalType, int numberOfElectron) {
+            this.orbitalType = orbitalType;
+            this.energyLevel = energyLevel;
+            this.numberOfElectron = numberOfElectron;
+        }
+
+        public String getOrbitalType() {
+            return orbitalType;
+        }
+
+        public int getEnergyLevel() {
+            return energyLevel;
+        }
+
+        public int getNumberOfElectron() {
+            return numberOfElectron;
+        }
+    }
+}
