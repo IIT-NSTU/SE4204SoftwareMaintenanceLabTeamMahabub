e1b450a867569038045c30435e170cbe660d4d35
Mahabub Alam <45809493+MahbubIT3@users.noreply.github.com>
2023-03-02 00:57:51 +0600

    Fixed Inappropriate Naming, Speculative Generality, Inappropriate Intimacy
    by Long Method, Inline Method, and Move method

--- ChemistryCalculator/frontend/HistoryFrame.java
+++ ChemistryCalculator/frontend/HistoryFrame.java
@@ -1,39 +1,35 @@
 package ChemistryCalculator.frontend;
 
+import ChemistryCalculator.backend.HistoryManager;
+
 import javax.swing.*;
-import javax.swing.filechooser.FileSystemView;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.io.File;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 
 
 public class HistoryFrame extends JFrame {
-    private static final Font SEGOE_UI = new Font("Segoe UI", Font.BOLD, 14);
-    private static final Color MAIN_COLOR = new Color(64, 43, 100);
-    private static final Color GRAY = new Color(204, 204, 204);
     private final JPanel mainPanel = new JPanel();
     private final JScrollPane mainScrollPane = new JScrollPane();
     private final JTextArea HistoryTextArea = new JTextArea();
     private final JButton clearButton = new JButton();
-    private final String filePath = FileSystemView.getFileSystemView().getDefaultDirectory().getPath() + File.separator
-            + "Chemistry Calculator" + File.separator
-            + "history.txt";
 
+    private final HistoryManager historyManager = new HistoryManager();
     public HistoryFrame() throws IOException {
         initComponents();
         setComponentLayout();
+        setFrameProperties();
         collectHistory();
     }
 
     private void setComponentLayout() {
-        //mainpanel Layout
-        GroupLayout mainPanelLayout = new GroupLayout(mainPanel);
-        mainPanel.setLayout(mainPanelLayout);
+        mainPanel.setLayout(getMainPanelLayout(mainPanel));
+        getContentPane().setLayout(getMainLayout(getContentPane()));
+    }
+
+    private LayoutManager getMainPanelLayout(Container container) {
+        GroupLayout mainPanelLayout = new GroupLayout(container);
         mainPanelLayout.setHorizontalGroup(
                 mainPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
                         .addGroup(GroupLayout.Alignment.TRAILING, mainPanelLayout.createSequentialGroup()
@@ -51,46 +47,49 @@ public class HistoryFrame extends JFrame {
                         .addComponent(clearButton, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
                         .addGap(20, 20, 20)
         );
-
-        //adding ainPanel to the frame
-        GroupLayout layout = new GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-                layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+        return mainPanelLayout;
+    }
+    
+    private LayoutManager getMainLayout(Container container) {
+        GroupLayout mainPanelLayout = new GroupLayout(container);
+        mainPanelLayout.setHorizontalGroup(
+                mainPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
                         .addComponent(mainPanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
-        layout.setVerticalGroup(
-                layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+        mainPanelLayout.setVerticalGroup(
+                mainPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
                         .addComponent(mainPanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
+        return mainPanelLayout;
     }
 
     private void initComponents() {
-        mainPanel.setBackground(Color.white);
-        mainScrollPane.setBorder(null);
+        setupClearButtonComponent();
+        setHistoryTextArea();
+    }
 
+    private void setHistoryTextArea() {
         HistoryTextArea.setFont(new Font("Segoe UI", Font.PLAIN, 15));
         HistoryTextArea.setLineWrap(true);
-
-        clearButton.setBackground(MAIN_COLOR);
-        clearButton.setFont(SEGOE_UI);
-        clearButton.setForeground(GRAY);
-        clearButton.setText("Clear HistoryManager");
-        clearButton.setAutoscrolls(true);
-        clearButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
-        clearButton.addActionListener(actionEvent -> {
-            try {
-                clearButtonActionPerformed(actionEvent);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-
         HistoryTextArea.setWrapStyleWord(true);
         HistoryTextArea.setBorder(null);
         HistoryTextArea.setMargin(new Insets(3, 5, 3, 3));
+        mainScrollPane.setBorder(null);
         mainScrollPane.setViewportView(HistoryTextArea);
+    }
 
+    private void setupClearButtonComponent() {
+        clearButton.setBackground(new Color(64, 43, 100));
+        clearButton.setFont(new Font("Segoe UI", Font.BOLD, 14));
+        clearButton.setForeground(new Color(204, 204, 204));
+        clearButton.setText("Clear HistoryManager");
+        clearButton.setAutoscrolls(true);
+        clearButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        clearButton.addActionListener(this::clearButtonActionPerformed);
+    }
+
+    private void setFrameProperties() {
+        mainPanel.setBackground(Color.white);
         setVisible(true);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setResizable(false);
@@ -100,24 +99,19 @@ public class HistoryFrame extends JFrame {
         setTitle("Equation Balance - HistoryManager");
     }
 
-    private void clearButtonActionPerformed(ActionEvent actionEvent) throws IOException {
-        File file = new File(filePath);
-        PrintWriter writer = new PrintWriter(file);
-        writer.print("");
-        writer.close();
+    private void clearButtonActionPerformed(ActionEvent actionEvent) {
+        historyManager.clearHistory();
         HistoryTextArea.setText("");
     }
 
-    public void collectHistory() throws IOException {
-        //sorting by date & time
-        ArrayList<String> temp = new ArrayList<>();
-        Files.lines(Paths.get(filePath)).forEach(temp::add);
-        if (temp.isEmpty()) {
+    public void collectHistory() {
+        ArrayList<String> historyList = historyManager.getHistory();
+        if (historyList.isEmpty()) {
             clearButton.setVisible(false);
             return;
         }
-        for (int i = temp.size() - 1; i >= 0; i--) {
-            HistoryTextArea.append(temp.get(i) + "\n");
+        for (int i = historyList.size() - 1; i >= 0; i--) {
+            HistoryTextArea.append(historyList.get(i) + "\n");
         }
     }
 
