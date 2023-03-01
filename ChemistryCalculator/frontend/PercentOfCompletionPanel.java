3e1f01bf289f99cfa3eb4199c3c90af5097b4d24
FaysalXulhash <46893200+FaysalXulhash@users.noreply.github.com>
2023-03-02 01:15:54 +0600

    Fixed Inappropriate Naming, Long Method, Inappropriate Naming, Comments by extract method

--- ChemistryCalculator/frontend/PercentOfCompletionPanel.java
+++ ChemistryCalculator/frontend/PercentOfCompletionPanel.java
@@ -6,6 +6,7 @@ import ChemistryCalculator.backend.InvalidAtomException;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -37,25 +38,69 @@ public class PercentOfCompletionPanel extends ContentPanel {
     }
 
     private void initComponent() {
-        labelforcompoundTextfield.setFont(SEGOE_UI);
-        labelforcompoundTextfield.setForeground(MAIN_COLOR);
-        labelforcompoundTextfield.setText("Enter Compound :");
+        setupCompoundLabelComponent();
+        setupPercentOfCompletionComponent();
+        setDataTableComponent();
+        setupChartButtonComponent();
+        setupErrorMessagePanel();
+        setupClearButtonComponent();
+    }
 
+    private void setupClearButtonComponent() {
+        clearButton.setBackground(MAIN_COLOR);
+        clearButton.setFont(SEGOE_UI);
+        clearButton.setForeground(GRAY);
+        clearButton.setText("Clear");
+        clearButton.setAutoscrolls(true);
+        clearButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        clearButton.addActionListener(this::clearActionPerformed);
+    }
+
+    private void setupErrorMessagePanel() {
+        errorMessagePanel.setBackground(Color.red);
+        errorMessagePanel.setVisible(false);
+        errorMessageLabel.setFont(SEGOE_UI);
+        errorMessageLabel.setForeground(Color.white);
+        errorMessageLabel.setHorizontalAlignment(SwingConstants.CENTER);
+    }
+
+    private void setupPercentOfCompletionComponent() {
         getPercentOfCompletionButton.setBackground(MAIN_COLOR);
         getPercentOfCompletionButton.setFont(SEGOE_UI);
         getPercentOfCompletionButton.setForeground(GRAY);
         getPercentOfCompletionButton.setText("Percent of completion");
         getPercentOfCompletionButton.setAutoscrolls(true);
         getPercentOfCompletionButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
-        getPercentOfCompletionButton.addActionListener(this::pcom_ans_buttonActionPerformed);
+        getPercentOfCompletionButton.addActionListener(this::percentageOfCompletionActionPerformed);
+    }
 
-        ansTableScrollPane.setVisible(false);
+    private void setupChartButtonComponent() {
+        pieChartButton.setVisible(false);
+        pieChartButton.setBackground(MAIN_COLOR);
+        pieChartButton.setFont(SEGOE_UI);
+        pieChartButton.setForeground(GRAY);
+        pieChartButton.setText("See pie chart");
+        pieChartButton.setAutoscrolls(true);
+        pieChartButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        pieChartButton.addActionListener(this::showPieChartActionPerformed);
+    }
 
+    private void setDataTableComponent() {
+        dataTableModel = getCustomDataTableModel();
         ansTable.setBackground(MAIN_COLOR);
         ansTable.setBorder(BorderFactory.createEtchedBorder());
         ansTable.setFont(SEGOE_UI);
         ansTable.setForeground(GRAY);
-        dataTableModel = new DefaultTableModel(
+        ansTable.setModel(dataTableModel);
+        ansTable.setCursor(new Cursor(Cursor.TEXT_CURSOR));
+        ansTable.setRowHeight(50);
+        ansTable.setShowVerticalLines(false);
+        ansTableScrollPane.setVisible(false);
+        ansTableScrollPane.setViewportView(ansTable);
+    }
+
+    private DefaultTableModel getCustomDataTableModel() {
+        return new DefaultTableModel(
                 new Object[][]{},
                 new String[]{}
         ) {
@@ -74,43 +119,67 @@ public class PercentOfCompletionPanel extends ContentPanel {
                 return canEdit[columnIndex];
             }
         };
+    }
 
-        ansTable.setModel(dataTableModel);
-        ansTable.setCursor(new Cursor(Cursor.TEXT_CURSOR));
-        ansTable.setRowHeight(50);
-        ansTable.setShowVerticalLines(false);
-        ansTableScrollPane.setViewportView(ansTable);
-
-        pieChartButton.setVisible(false);
-        pieChartButton.setBackground(MAIN_COLOR);
-        pieChartButton.setFont(SEGOE_UI);
-        pieChartButton.setForeground(GRAY);
-        pieChartButton.setText("See pie chart");
-        pieChartButton.setAutoscrolls(true);
-        pieChartButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
-        pieChartButton.addActionListener(this::pcom_pie_chart_buttonActionPerformed);
-
-        errorMessagePanel.setBackground(Color.red);
-        errorMessagePanel.setVisible(false);
-
-
-        errorMessageLabel.setFont(SEGOE_UI);
-        errorMessageLabel.setForeground(Color.white);
-        errorMessageLabel.setHorizontalAlignment(SwingConstants.CENTER);
-
-        clearButton.setBackground(MAIN_COLOR);
-        clearButton.setFont(SEGOE_UI);
-        clearButton.setForeground(GRAY);
-        clearButton.setText("Clear");
-        clearButton.setAutoscrolls(true);
-        clearButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
-        clearButton.addActionListener(this::pcom_clear_buttonActionPerformed);
+    private void setupCompoundLabelComponent() {
+        labelforcompoundTextfield.setFont(SEGOE_UI);
+        labelforcompoundTextfield.setForeground(MAIN_COLOR);
+        labelforcompoundTextfield.setText("Enter Compound :");
     }
 
     private void setComponentLayout() {
-        //error Message Panel Layout
+        errorMessagePanel.setLayout(getErrorMessagePanelLayout());
+        this.setLayout(getMainLayout(this));
+    }
+
+    private LayoutManager getMainLayout(Container container) {
+        GroupLayout mainLayout = new GroupLayout(container);
+        mainLayout.setHorizontalGroup(
+                mainLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                        .addGroup(mainLayout.createSequentialGroup()
+                                .addComponent(pieChartButton)
+                                .addGap(0, 0, Short.MAX_VALUE))
+                        .addGroup(mainLayout.createSequentialGroup()
+                                .addComponent(ansTableScrollPane, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addContainerGap())
+                        .addGroup(mainLayout.createSequentialGroup()
+                                .addGap(40, 40, 40)
+                                .addGroup(mainLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                                        .addComponent(errorMessagePanel, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                                        .addGroup(mainLayout.createSequentialGroup()
+                                                .addComponent(getPercentOfCompletionButton)
+                                                .addGap(18, 18, 18)
+                                                .addComponent(clearButton))
+                                        .addGroup(mainLayout.createSequentialGroup()
+                                                .addComponent(labelforcompoundTextfield)
+                                                .addGap(18, 18, 18)
+                                                .addComponent(compoundTextfield, GroupLayout.PREFERRED_SIZE, 270, GroupLayout.PREFERRED_SIZE)))
+                                .addContainerGap(93, Short.MAX_VALUE))
+        );
+        mainLayout.setVerticalGroup(
+                mainLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                        .addGroup(mainLayout.createSequentialGroup()
+                                .addGap(31, 31, 31)
+                                .addComponent(errorMessagePanel, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addGroup(mainLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                                        .addComponent(labelforcompoundTextfield, GroupLayout.PREFERRED_SIZE, 36, GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(compoundTextfield, GroupLayout.PREFERRED_SIZE, 43, GroupLayout.PREFERRED_SIZE))
+                                .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addGroup(mainLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                                        .addComponent(getPercentOfCompletionButton, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(clearButton, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE))
+                                .addGap(64, 64, 64)
+                                .addComponent(ansTableScrollPane, GroupLayout.PREFERRED_SIZE, 257, GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addComponent(pieChartButton, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
+                                .addContainerGap(72, Short.MAX_VALUE))
+        );
+        return mainLayout;
+    }
+
+    private LayoutManager getErrorMessagePanelLayout() {
         GroupLayout errorMessagePanelLayout = new GroupLayout(errorMessagePanel);
-        errorMessagePanel.setLayout(errorMessagePanelLayout);
         errorMessagePanelLayout.setHorizontalGroup(
                 errorMessagePanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
                         .addGroup(GroupLayout.Alignment.TRAILING, errorMessagePanelLayout.createSequentialGroup()
@@ -123,65 +192,18 @@ public class PercentOfCompletionPanel extends ContentPanel {
                                 .addComponent(errorMessageLabel, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
                                 .addGap(0, 0, Short.MAX_VALUE))
         );
-
-        //main panel Layout
-        GroupLayout Layout = new GroupLayout(this);
-        this.setLayout(Layout);
-        Layout.setHorizontalGroup(
-                Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                        .addGroup(Layout.createSequentialGroup()
-                                .addComponent(pieChartButton)
-                                .addGap(0, 0, Short.MAX_VALUE))
-                        .addGroup(Layout.createSequentialGroup()
-                                .addComponent(ansTableScrollPane, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addContainerGap())
-                        .addGroup(Layout.createSequentialGroup()
-                                .addGap(40, 40, 40)
-                                .addGroup(Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                                        .addComponent(errorMessagePanel, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-                                        .addGroup(Layout.createSequentialGroup()
-                                                .addComponent(getPercentOfCompletionButton)
-                                                .addGap(18, 18, 18)
-                                                .addComponent(clearButton))
-                                        .addGroup(Layout.createSequentialGroup()
-                                                .addComponent(labelforcompoundTextfield)
-                                                .addGap(18, 18, 18)
-                                                .addComponent(compoundTextfield, GroupLayout.PREFERRED_SIZE, 270, GroupLayout.PREFERRED_SIZE)))
-                                .addContainerGap(93, Short.MAX_VALUE))
-        );
-        Layout.setVerticalGroup(
-                Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                        .addGroup(Layout.createSequentialGroup()
-                                .addGap(31, 31, 31)
-                                .addComponent(errorMessagePanel, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-                                .addGap(18, 18, 18)
-                                .addGroup(Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                                        .addComponent(labelforcompoundTextfield, GroupLayout.PREFERRED_SIZE, 36, GroupLayout.PREFERRED_SIZE)
-                                        .addComponent(compoundTextfield, GroupLayout.PREFERRED_SIZE, 43, GroupLayout.PREFERRED_SIZE))
-                                .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addGroup(Layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                                        .addComponent(getPercentOfCompletionButton, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
-                                        .addComponent(clearButton, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE))
-                                .addGap(64, 64, 64)
-                                .addComponent(ansTableScrollPane, GroupLayout.PREFERRED_SIZE, 257, GroupLayout.PREFERRED_SIZE)
-                                .addGap(18, 18, 18)
-                                .addComponent(pieChartButton, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
-                                .addContainerGap(72, Short.MAX_VALUE))
-        );
-
+        return errorMessagePanelLayout;
     }
 
-    private void pcom_ans_buttonActionPerformed(java.awt.event.ActionEvent evt) {
-        String compound_text = compoundTextfield.getText();
-        if (!compound_text.isEmpty()) {
+    private void percentageOfCompletionActionPerformed(ActionEvent evt) {
+        String compoundText = compoundTextfield.getText();
+        if (!compoundText.isEmpty()) {
             Compound compound;
             try {
-                compound = new Compound(compound_text);
-
+                compound = new Compound(compoundText);
             } catch (InvalidAtomException e) {
                 errorMessagePanel.setVisible(true);
                 errorMessageLabel.setText(e.getMessage());
-
 
                 pieChartButton.setVisible(false);
                 ansTableScrollPane.setVisible(false);
@@ -214,14 +236,14 @@ public class PercentOfCompletionPanel extends ContentPanel {
         }
     }
 
-    private void pcom_clear_buttonActionPerformed(java.awt.event.ActionEvent evt) {
+    private void clearActionPerformed(ActionEvent event) {
         ansTableScrollPane.setVisible(false);
         pieChartButton.setVisible(false);
         errorMessagePanel.setVisible(false);
         compoundTextfield.setText(null);
     }
 
-    private void pcom_pie_chart_buttonActionPerformed(java.awt.event.ActionEvent evt) {
+    private void showPieChartActionPerformed(ActionEvent event) {
         FxPieChart pieChart = new FxPieChart(dataTableModel.getDataVector());
         pieChart.toFront();
         pieChart.requestFocus();
