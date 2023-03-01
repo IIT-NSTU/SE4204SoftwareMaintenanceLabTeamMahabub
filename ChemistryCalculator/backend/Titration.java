ae4d598d8a84a226770f619b99776e844e24dfba
Moon Moon Das <46723130+MoonMoonDas@users.noreply.github.com>
2023-03-02 01:46:24 +0600

    Fixed primitive obsession, extra comments, Long Method, Long Parameter List, inappropriate naming by extract method, extract class

--- ChemistryCalculator/backend/Titration.java
+++ ChemistryCalculator/backend/Titration.java
@@ -3,67 +3,32 @@ package ChemistryCalculator.backend;
 import java.util.Arrays;
 
 public class Titration {
-
-    private final String[] acidProperties = new String[3];
-    private final String[] baseProperties = new String[3];
-    private boolean unknownValue_in_Acid = false;
+    private TitrationProperties acidProperties = new TitrationProperties();
+    private TitrationProperties baseProperties = new TitrationProperties();
+    private boolean hasUnknownValueInAcid = false;
 
     //Here one and only one property can be empty, which is the unknown value
-    public Titration(String molarityOfAcid,
-                     String molarityOfBase,
-                     String volumeOfAcid,
-                     String volumeOfBase,
-                     String numOfMolesOfAcid,
-                     String numOfMolesOfBase) {
-        acidProperties[0] = molarityOfAcid;
-        acidProperties[1] = volumeOfAcid;
-        acidProperties[2] = numOfMolesOfAcid;
-
-        baseProperties[0] = molarityOfBase;
-        baseProperties[1] = volumeOfBase;
-        baseProperties[2] = numOfMolesOfBase;
+    public Titration(TitrationProperties acidProperties, TitrationProperties baseProperties) {
+        this.acidProperties = acidProperties;
+        this.baseProperties = baseProperties;
     }
 
-    //Executing formula  =>  (numOfMolesOfBase*molarityOfBase*volumeOfBase) = numOfMolesOfAcid*molarityOfAcid*volumeOfAcid)
     public double getUnknownValue() {
         if (this.isValid()) {
             double numerator;
             double denominator = 1;
-            if (unknownValue_in_Acid) {
-                numerator = Arrays.stream(baseProperties).mapToDouble(Double::parseDouble).reduce(1, (a, b) -> a * b);
-                for (String property : acidProperties) {
-                    if (!property.isEmpty()) {
-                        denominator *= Double.parseDouble(property);
-                    }
-                }
-                return numerator / denominator;
+            if (hasUnknownValueInAcid) {
+                return baseProperties.getConcentration() / acidProperties.getConcentration();
             } else {
-                numerator = Arrays.stream(acidProperties).mapToDouble(Double::parseDouble).reduce(1, (a, b) -> a * b);
-                for (String property : baseProperties) {
-                    if (!property.isEmpty()) {
-                        denominator *= Double.parseDouble(property);
-                    }
-                }
-
-                return numerator / denominator;
+                return acidProperties.getConcentration() / baseProperties.getConcentration();
             }
         } else {
             throw new InsufficientDataException("\"Fill up any 5 fields to get unknown value\"");
         }
     }
 
-
-    // if there is only one index value contain empty string  in both String[] acidProperties and String[] baseProperties, then its return true. False otherwise
     private boolean isValid() {
-        int count = 0;
-        for (String property : acidProperties) {
-            if (property.isEmpty()) {
-                unknownValue_in_Acid = true;
-                count++;
-            }
-        }
-        count += Arrays.stream(baseProperties).filter(String::isEmpty).count();
-        return count < 2;
+        return acidProperties.countInvalid() + baseProperties.countInvalid() < 2;
     }
 
 }
